6ac7325a39d2068b8226c229fd61813eb7c04bba
nullfjorten <43184980+nullfjorten@users.noreply.github.com>
2021-05-09 23:20:03 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,39 +8,45 @@
 // }
 function drawSnake () {
     for (let pixel of snake) {
-        led.plot(0, 0)
+        led.plot(pixel[0], pixel[1])
     }
 }
 let x = 0
 let y = 0
-let snake: number[] = []
+let snake: number[][] = []
 snake = []
 let direction = 2
-let sensitivity = 200
+let sensitivity = 100
+let showDebug = false
 basic.forever(function () {
-    let list: number[][] = []
     // right
     if (input.acceleration(Dimension.X) > sensitivity) {
         direction = 2
-        led.plot(4, 2)
+        if (showDebug == true) {
+            led.plot(4, 2)
+        }
     }
     // down
     if (input.acceleration(Dimension.Y) > sensitivity) {
         direction = 3
-        led.plot(2, 4)
+        if (showDebug == true) {
+            led.plot(2, 4)
+        }
     }
     // left
     if (input.acceleration(Dimension.X) < sensitivity * -1) {
         direction = 4
-        led.plot(0, 2)
+        if (showDebug == true) {
+            led.plot(0, 2)
+        }
     }
     // up
     if (input.acceleration(Dimension.Y) < sensitivity * -1) {
         direction = 1
-        led.plot(2, 0)
+        if (showDebug == true) {
+            led.plot(2, 0)
+        }
     }
-    basic.pause(500)
-    basic.clearScreen()
     if (direction == 1) {
         y += -1
     }
@@ -53,6 +59,11 @@ basic.forever(function () {
     if (direction == 4) {
         x += -1
     }
-    list.push([x, y])
+    snake.push([x, y])
     drawSnake()
+    if (x < 0 || x > 4 || y < 0 || y > 4) {
+        game.gameOver()
+    }
+    basic.pause(1000)
+    basic.clearScreen()
 })
